b09145b4b765a7fd13342306907e65bfef69beb3
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2020-01-26 14:18:08 +0100

    add first module
    - understanding of import
    - understanding of types in function header

---
+++ 00002_first_module/main.py
@@ -0,0 +1,12 @@
+import mymathmodule
+
+
+def main():
+    print("add 1, 2 =>", mymathmodule.add(1, 2))
+    print("sub 2, 1 =>", mymathmodule.sub(2, 1))
+    print("mul 2, 2 =>", mymathmodule.mul(2, 2))
+    print("div 4, 2 =>", mymathmodule.div(4, 2))
+
+
+if __name__ == "__main__":
+    main()
